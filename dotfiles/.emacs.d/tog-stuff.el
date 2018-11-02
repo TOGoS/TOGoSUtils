3f86885937a3781f742596f866ac6dc23f13d6fd
TOGoS <togos00@gmail.com>
2018-11-01 19:04:08 -0500

    Set default buffer file coding system to utf-8-unix in tog-stuff.el

--- dotfiles/.emacs.d/tog-stuff.el
+++ dotfiles/.emacs.d/tog-stuff.el
@@ -1,3 +1,5 @@
+(setq default-buffer-file-coding-system 'utf-8-unix)
+
 (let ((lode (lambda (filename) (load-file (concat (file-name-directory (or load-file-name buffer-file-name)) "/" filename)))))
   (funcall lode "tog-functions.el")
   (funcall lode "tog-hooks.el")
