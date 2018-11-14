51fcd2caffaa354cab20501f5f801a40afd713ad
TOGoS <togos00@gmail.com>
2018-11-14 09:30:14 -0600

    setq-default buffer-file-coding-system 'utf-8-unix

--- dotfiles/.emacs.d/tog-stuff.el
+++ dotfiles/.emacs.d/tog-stuff.el
@@ -1,4 +1,4 @@
-(setq default-buffer-file-coding-system 'utf-8-unix)
+(setq-default buffer-file-coding-system 'utf-8-unix)
 
 (let ((lode (lambda (filename) (load-file (concat (file-name-directory (or load-file-name buffer-file-name)) "/" filename)))))
   (funcall lode "tog-functions.el")
