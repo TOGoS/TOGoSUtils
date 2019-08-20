f46839dfa4f6082f941e41b49903e34a501241b1
TOGoS <togos00@gmail.com>
2019-08-20 12:21:56 -0500

    Include TOGoSMusicMetadata in morning pull

--- dotfiles/.emacs.d/tog-hooks.el
+++ dotfiles/.emacs.d/tog-hooks.el
@@ -1,2 +1,9 @@
 (add-hook 'c++-mode-hook 'infer-indentation-style)
 (add-hook 'text-mode-hook 'tog-tabs-8)
+
+;; Seems like this should be default!
+;; Maybe one of my other scripts messes it up?
+;; But this seems to fix the problem:
+(defun tog-org-mode-hook ()
+  (local-set-key "\t" 'org-cycle))
+(add-hook 'org-mode-hook 'tog-org-mode-hook)
