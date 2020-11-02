89d354cd20b4123bcfe512149495b6c507e3504e
TOGoS <togos00@gmail.com>
2020-11-01 22:36:08 -0600

    delete-selection-mode

--- dotfiles/.emacs.d/tog-stuff.el
+++ dotfiles/.emacs.d/tog-stuff.el
@@ -1,9 +1,8 @@
 (setq-default buffer-file-coding-system 'utf-8-unix)
+(setq delete-selection-mode t) ; So I can overwrite stuff without polluting the clipboard
 
 (let ((lode (lambda (filename) (load-file (concat (file-name-directory (or load-file-name buffer-file-name)) "/" filename)))))
   (funcall lode "tog-functions.el")
   (funcall lode "tog-keys.el")
   (funcall lode "tog-hooks.el")
   (funcall lode "tog-org-stuff.el"))
-
-
