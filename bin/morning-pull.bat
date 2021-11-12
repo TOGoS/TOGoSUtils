c0e4f683732e9a67fdcac06545fda2bfcbbb532c
TOGoS <togos00@gmail.com>
2021-11-12 09:15:43 -0600

    morning-pull.bat --own-window

--- bin/morning-pull.bat
+++ bin/morning-pull.bat
@@ -7,11 +7,13 @@ set "self_dir=%~dp0"
 set "pause_if_errors="
 rem `shift` doesn't affect %*.  So do it like this:
 set "remaining_args="
+set "own_window="
 
 
 :process_arg
 if "" == "%1" goto no_more_args
-if "--pause-if-errors" == "%~1%" goto parse_pause_if_errors
+if "--pause-if-errors" == "%~1" (set "pause_if_errors=1" & goto process_next_arg)
+if "--own-window" == "%~1" (set "own_window=1" & set "pause_if_errors=1" & goto process_next_arg)
 rem Otherwise add it to the list
 rem echo # set "remaining_args=%remaining_args% %1"
 set "remaining_args=%remaining_args% %1"
@@ -21,17 +23,15 @@ shift
 goto process_arg
 
 
-:parse_pause_if_errors
-echo Thank you for choosing --pause-if-error
-set "pause_if_errors=1"
-goto process_next_arg
-
-
 :no_more_args
+
+if defined own_window title Morning pull!
+
 rem echo Remaining args: %remaining_args%
 rem echo # Running: node %self_dir%morning-pull%remaining_args%
 node %self_dir%morning-pull%remaining_args%
 if errorlevel 1 goto fail
+if defined own_window exit 0
 goto eof
 
 
@@ -44,6 +44,7 @@ goto fail_exit
 
 
 :fail_exit
+if defined own_window exit 1
 exit /B 1
 
 
