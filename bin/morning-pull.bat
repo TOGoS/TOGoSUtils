7941b18996ee9c1596036f6354ec80474c19e3f9
TOGoS <togos00@gmail.com>
2021-11-11 22:43:56 -0600

    morning-pull.bat --pause-if-errors

--- bin/morning-pull.bat
+++ bin/morning-pull.bat
@@ -1,1 +1,48 @@
-@node %~dp0\morning-pull %*
+@echo off
+
+setlocal
+
+set "self_name=%~nx0"
+set "self_dir=%~dp0"
+set "pause_if_errors="
+rem `shift` doesn't affect %*.  So do it like this:
+set "remaining_args="
+
+
+:process_arg
+if "%1" == "" goto no_more_args
+if "%~1%" == "--pause-if-errors" goto parse_pause_if_errors
+rem Otherwise add it to the list
+set "remaining_args=%remaining_args% %1"
+:process_next_arg
+shift
+goto process_arg
+
+
+:parse_pause_if_errors
+echo Thank you for choosing --pause-if-error
+set "pause_if_errors=1"
+goto process_next_arg
+
+
+:no_more_args
+rem echo Remaining args: %remaining_args%
+node %self_dir%morning-pull%remaining_args%
+if errorlevel 1 goto fail
+goto eof
+
+
+:fail
+echo %self_name%: Oh no, there were some errors.
+if not defined pause_if_errors goto fail_exit
+echo %self_name%: Pausing because --pause-if-error
+pause
+goto fail_exit
+
+
+:fail_exit
+exit /B 1
+
+
+
+:eof
