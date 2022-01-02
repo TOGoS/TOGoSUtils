42e95399b5f2077d581826461ad44d2936bdedbb
TOGoS <togos00@gmail.com>
2022-01-01 23:02:10 -0600

    ccouch3-cache.bat

--- bin/ccouch3-cache.bat
+++ bin/ccouch3-cache.bat
@@ -6,7 +6,16 @@ rem Jot that down for new versions.
 rem Anyway, set CCOUCH3_REMOTE_ARGS in your environment to make it work good.
 
 setlocal
+call require-ccouch-env.bat
+if errorlevel 1 goto fail
 
 if not defined CCOUCH3_REMOTE_ARGS set "CCOUCH3_REMOTE_ARGS="
 
-ccouch3 cache %CCOUCH3_REMOTE_ARGS% %*
+java -jar %ccouch3_jar% cache %CCOUCH3_REMOTE_ARGS% %*
+if errorlevel 1 goto fail
+goto eof
+
+:fail
+echo %~f0 exiting with errorlevel 1 due to errors >&2
+exit /B 1
+:eof
