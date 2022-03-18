186240889d5d40d64c716e042bd92d0cde487e7b
TOGoS <togos00@gmail.com>
2022-03-18 14:56:59 -0500

    Fix ccouch3.bat's ccouch3_jar defaulting

--- bin/ccouch3.bat
+++ bin/ccouch3.bat
@@ -2,8 +2,10 @@
 
 setlocal
 
+set "ccouch3_jar_guess1=%UserProfile%\stuff\proj\ContentCouch3\CCouch3.jar"
+
 if defined ccouch3_jar goto ccouch3_jar_found
-if exist "%UserProfile%\stuff\proj\ContentCouch3\CCouch3.jar" (set "ccouch3_jar=%UserProfile%\stuff\proj\ContentCouch\CCouch3.jar" & goto ccouch3_jar_found)
+if exist "%ccouch3_jar_guess1%" (set "ccouch3_jar=%ccouch3_jar_guess1%" & goto ccouch3_jar_found)
 
 echo Couldn't find CCouch3.jar.  Try setting the ccouch3_jar environment variable. >&2
 exit /b 1
