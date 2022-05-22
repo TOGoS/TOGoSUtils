e118bb903b00499ab773bafd1016dbfaa5886d4e
TOGoS <togos00@gmail.com>
2022-05-22 12:52:27 -0500

    ccouch3-upload-to-marvin: Pass `-batch` to plink

--- bin/ccouch3-upload-to-marvin.bat
+++ bin/ccouch3-upload-to-marvin.bat
@@ -11,8 +11,8 @@ set command=java -jar %ccouch3_jar% upload -repo:%ccouch_repo_name% %ccouch_repo
 
 set any_remotes=0
 
-if defined fs_marvin_ssh_port (set command=%command% -command-server:fs.marvin plink -P %fs_marvin_ssh_port% tog@%fs_marvin_ssh_hostname% ccouch3 command-server -repo ~/.ccouch -sector %ccouch_store_sector% ";" && set any_remotes=1)
-if defined togos_fbs_ssh_port (set command=%command% -command-server:togos-fbs plink -P %togos_fbs_ssh_port% tog@%togos_fbs_ssh_hostname% ccouch3 command-server -repo ~/.ccouch -sector %ccouch_store_sector% ";" && set any_remotes=1)
+if defined fs_marvin_ssh_port (set command=%command% -command-server:fs.marvin plink -batch -P %fs_marvin_ssh_port% tog@%fs_marvin_ssh_hostname% ccouch3 command-server -repo ~/.ccouch -sector %ccouch_store_sector% ";" && set any_remotes=1)
+if defined togos_fbs_ssh_port (set command=%command% -command-server:togos-fbs plink -batch -P %togos_fbs_ssh_port% tog@%togos_fbs_ssh_hostname% ccouch3 command-server -repo ~/.ccouch -sector %ccouch_store_sector% ";" && set any_remotes=1)
 
 if %any_remotes% EQU 0 (echo No remotes specified; plz set fs_marvin_ssh_port and/or togos_fbs_ssh_port)
 
