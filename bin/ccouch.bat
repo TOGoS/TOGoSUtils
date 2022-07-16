89f82cdaedae8bf5910cfa16ade1a25b67e4ac80
TOGoS <togos00@gmail.com>
2022-07-16 16:05:08 -0500

    ccouch.bat uses CCOUCH_REMOTE_ARGS, and other vars now capitalized

--- bin/ccouch.bat
+++ bin/ccouch.bat
@@ -2,10 +2,10 @@
 
 setlocal
 
-if defined ccouch_jar goto ccouch_jar_found
-if exist "%UserProfile%\stuff\proj\ContentCouch\ContentCouch.jar" (set "ccouch_jar=%UserProfile%\stuff\proj\ContentCouch\ContentCouch.jar" & goto ccouch_jar_found)
+if defined CCOUCH_JAR goto ccouch_jar_found
+if exist "%UserProfile%\stuff\proj\ContentCouch\ContentCouch.jar" (set "CCOUCH_JAR=%UserProfile%\stuff\proj\ContentCouch\ContentCouch.jar" & goto ccouch_jar_found)
 
-echo Couldn't find ContentCouch.jar.  Try setting the ccouch_jar environment variable. >&2
+echo Couldn't find ContentCouch.jar.  Try setting the CCOUCH_JAR environment variable. >&2
 exit /b 1
 
 
@@ -13,19 +13,20 @@ exit /b 1
 
 set "ccouch_args="
 
-if not defined ccouch_repo_dir goto end_build_repo_arg
-if not defined ccouch_repo_name goto end_build_repo_arg
+if not defined CCOUCH_REPO_DIR goto end_build_repo_arg
+if not defined CCOUCH_REPO_NAME goto end_build_repo_arg
+if not defined CCOUCH_REMOTE_ARGS set "CCOUCH_REMOTE_ARGS="
 
 rem I replace backslash with slash here because OTHERWISE A TRAILING BACKSLASH
 rem GETS INTERPRETED AS AN ESCAPE SEQUENCE AND ccouch WILL COMPLAIN THAT 'No command given'.
-rem This can also be worked aound by appending a '.' (e.g. `set ccouch_repo_dir=%~dp0.` in .env.bat)
+rem This can also be worked aound by appending a '.' (e.g. `set CCOUCH_REPO_DIR=%~dp0.` in .env.bat)
 rem but I figure just switching them to forward slashes is a bit more foolproof.
 
-set "ccouch_args=-repo:%ccouch_repo_name% "%ccouch_repo_dir:\=/%""
+set "ccouch_args=-repo:%CCOUCH_REPO_NAME% "%CCOUCH_REPO_DIR:\=/%" %CCOUCH_REMOTE_ARGS%"
 
 :end_build_repo_arg
 
-set "cmd=java -jar "%ccouch_jar%" %ccouch_args% %*"
+set "cmd=java -jar "%CCOUCH_JAR%" %ccouch_args% %*"
 
-rem echo %cmd%
+echo %cmd%
 %cmd%
