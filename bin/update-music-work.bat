45ed35daf4afe7cae846a7d007f4a01537963453
TOGoS <togos00@gmail.com>
2022-08-19 00:00:06 -0500

    Also pull music work from Framey

--- bin/update-music-work.bat
+++ bin/update-music-work.bat
@@ -7,12 +7,16 @@ if defined tog_stuff_dir (set music_work_dir=%tog_stuff_dir%\music\work & goto f
 echo %self_name%: Error: Neither music_work_dir nor tog_stuff_dir is set >&2 & goto fail)
 :find_music_work_dir_done
 
-call ccouch cache-heads //fs.marvin/togthoms1/ //togos-fbs/togthoms1/ //fs.marvin/wsitem-3306.1/ //togos-fbs/wsitem-3306.1/
+call ccouch cache-heads ^
+	//fs.marvin/togthoms1/ //togos-fbs/togthoms1/ ^
+	//fs.marvin/wsitem-3306.1/ //togos-fbs/wsitem-3306.1/ ^
+	//fs.marvin/framey-2021/ //togos-fbs/framey-2021/
 if errorlevel 1 goto fail
-call ccouch3-cache -recurse -sector music x-ccouch-head:togthoms1/tog/music/work/latest x-ccouch-head:wsitem-3306.1/tog/music/work/latest
+call ccouch3-cache -recurse -sector music x-ccouch-head:togthoms1/tog/music/work/latest x-ccouch-head:wsitem-3306.1/tog/music/work/latest x-ccouch-head:framey-2021/tog/music/work/latest
 if errorlevel 1 goto fail
 call ccouch checkout -link -merge x-rdf-subject:x-ccouch-head:togthoms1/tog/music/work/latest %music_work_dir%/
 call ccouch checkout -link -merge x-rdf-subject:x-ccouch-head:wsitem-3306.1/tog/music/work/latest %music_work_dir%/
+call ccouch checkout -link -merge x-rdf-subject:x-ccouch-head:framey-2021/tog/music/work/latest %music_work_dir%/
 if errorlevel 1 goto fail
 
 :stdbatfooter
