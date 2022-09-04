9964ea949b57a69ee8fb08405c0346eeb5fd80d5
TOGoS <togos00@gmail.com>
2022-09-04 13:58:33 -0500

    Reference deno.land version of list-todo.ts instead of including in this repo

--- bin/taskme.bat
+++ bin/taskme.bat
@@ -1,6 +1,8 @@
 @echo off
 setlocal
-set "self_dir=%~dp0"
 
 cls
-cat %UserProfile%\stuff\job\EarthIT\timelog\project-tasks.tef %UserProfile%\stuff\music\TOGoSMusicMetadata\tasks.tef | deno run "%self_dir%list-todo.ts" -p
+cat ^
+		%UserProfile%\stuff\job\EarthIT\timelog\project-tasks.tef ^
+		%UserProfile%\stuff\music\TOGoSMusicMetadata\tasks.tef ^
+	| deno run "https://deno.land/x/listtodo27@v0.1.1/list-todo.ts" -p
