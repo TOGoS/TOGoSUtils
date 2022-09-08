3c2a2fe6dbe824769f73643b931095ddbf2357d6
TOGoS <togos00@gmail.com>
2022-09-08 12:48:28 -0500

    taskme.bat: Use listtodo27@v0.1.3

--- bin/taskme.bat
+++ bin/taskme.bat
@@ -5,4 +5,4 @@ cls
 cat ^
 		%UserProfile%\stuff\job\EarthIT\timelog\project-tasks.tef ^
 		%UserProfile%\stuff\music\TOGoSMusicMetadata\tasks.tef ^
-	| deno run "https://deno.land/x/listtodo27@v0.1.1/list-todo.ts" -p
+	| deno run "https://deno.land/x/listtodo27@v0.1.3/list-todo.ts" -p
