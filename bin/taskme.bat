722e3c24f70635e9d748f75fa80a12344c54e2e8
TOGoS <togos00@gmail.com>
2022-09-14 11:12:05 -0500

    taskme.bat: Also bring in tasks from ProjectNotes2/2022/Projects.tef

--- bin/taskme.bat
+++ bin/taskme.bat
@@ -4,5 +4,6 @@ setlocal
 cls
 cat ^
 		%UserProfile%\stuff\job\EarthIT\timelog\project-tasks.tef ^
+		%UserProfile%\stuff\docs\ProjectNotes2\2022\Projects.tef ^
 		%UserProfile%\stuff\music\TOGoSMusicMetadata\tasks.tef ^
-	| deno run "https://deno.land/x/listtodo27@v0.1.3/list-todo.ts" -p
+	| deno run "https://deno.land/x/listtodo27@v0.1.3/list-todo.ts" -p %*
