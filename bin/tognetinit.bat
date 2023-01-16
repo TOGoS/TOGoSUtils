1050241db2311d534ce78d251c0e8b5695d57cab
TOGoS <togos00@gmail.com>
2023-01-16 09:16:03 -0600

    tognetinit.bat: 'set' as well as 'setx' variables

--- bin/tognetinit.bat
+++ bin/tognetinit.bat
@@ -1,28 +1,34 @@
 @echo off
 
-if "%1"=="4909" goto init_4909net
-if "%1"=="internet" goto init_internet
+if "%~1"=="4909" goto init_4909net
+if "%~1"=="internet" goto init_internet
 echo Please specify '4909' or 'internet'>&2
 exit /B 1
 
 :init_4909net
 
-setx FS_MARVIN_SSH_PORT 22
-setx FS_MARVIN_SSH_HOSTNAME 192.168.9.8
-setx TOGOS_FBS_SSH_PORT 22
-setx TOGOS_FBS_SSH_HOSTNAME 192.168.9.15
+set "FS_MARVIN_SSH_PORT=22"
+set "FS_MARVIN_SSH_HOSTNAME=192.168.9.8"
+set "TOGOS_FBS_SSH_PORT=22"
+set "TOGOS_FBS_SSH_HOSTNAME=192.168.9.15"
 
-goto eof
+goto setx
 
 
 :init_internet
 
-setx FS_MARVIN_SSH_PORT 22
-setx FS_MARVIN_SSH_HOSTNAME fs.marvin.nuke24.net
-setx TOGOS_FBS_SSH_PORT 31522
-setx TOGOS_FBS_SSH_HOSTNAME external.marvin.nuke24.net
+set "FS_MARVIN_SSH_PORT=22"
+set "FS_MARVIN_SSH_HOSTNAME=fs.marvin.nuke24.net"
+set "TOGOS_FBS_SSH_PORT=31522"
+set "TOGOS_FBS_SSH_HOSTNAME=external.marvin.nuke24.net"
 
-goto eof
+goto setx
 
+
+:setx
+setx FS_MARVIN_SSH_PORT %FS_MARVIN_SSH_PORT%
+setx FS_MARVIN_SSH_HOSTNAME %FS_MARVIN_SSH_HOSTNAME%
+setx TOGOS_FBS_SSH_PORT %TOGOS_FBS_SSH_PORT%
+setx TOGOS_FBS_SSH_HOSTNAME %TOGOS_FBS_SSH_HOSTNAME%
 
 :eof
