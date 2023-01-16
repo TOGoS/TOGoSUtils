e41c087d38fd702848151abe0a31686d5911e41f
TOGoS <togos00@gmail.com>
2023-01-16 09:12:39 -0600

    Don't default {fs_marvin,togos_fbs}_ssh_{host,port}
    - require-ccouch-env.bat: Don't default those variables
    - ccouch3-upload-to-marvin.bat: Allow <host>_ssh_port to be unset

--- bin/ccouch3-upload-to-marvin.bat
+++ bin/ccouch3-upload-to-marvin.bat
@@ -11,10 +11,12 @@ set command=java -jar %ccouch3_jar% upload -repo:%ccouch_repo_name% %ccouch_repo
 
 set any_remotes=0
 
-if defined fs_marvin_ssh_port (set command=%command% -command-server:fs.marvin plink -batch -P %fs_marvin_ssh_port% tog@%fs_marvin_ssh_hostname% ccouch3 command-server -repo ~/.ccouch -sector %ccouch_store_sector% ";" && set any_remotes=1)
-if defined togos_fbs_ssh_port (set command=%command% -command-server:togos-fbs plink -batch -P %togos_fbs_ssh_port% tog@%togos_fbs_ssh_hostname% ccouch3 command-server -repo ~/.ccouch -sector %ccouch_store_sector% ";" && set any_remotes=1)
+if defined fs_marvin_ssh_port set "portarg=-P %fs_marvin_ssh_port%"
+if defined fs_marvin_ssh_hostname (set command=%command% -command-server:fs.marvin plink -batch -P %fs_marvin_ssh_port% tog@%fs_marvin_ssh_hostname% ccouch3 command-server -repo ~/.ccouch -sector %ccouch_store_sector% ";" && set any_remotes=1)
+if defined togos_fbs_ssh_port set "portarg=-P %togos_fbs_ssh_port%"
+if defined togos_fbs_ssh_hostname (set command=%command% -command-server:togos-fbs plink -batch %portarg% tog@%togos_fbs_ssh_hostname% ccouch3 command-server -repo ~/.ccouch -sector %ccouch_store_sector% ";" && set any_remotes=1)
 
-if %any_remotes% EQU 0 (echo No remotes specified; plz set fs_marvin_ssh_port and/or togos_fbs_ssh_port)
+if %any_remotes% EQU 0 (echo No remotes specified; plz set fs_marvin_ssh_hostname [and _port] and/or togos_fbs_ssh_hostname [and _port])
 
 set command=%command% %*
 
