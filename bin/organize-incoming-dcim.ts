c82ed6848030c0cb48cf54e0b8e7b8581e254a68
TOGoS <togos00@gmail.com>
2023-05-13 16:59:38 -0500

    organize-incoming-dcim can now dump help text

--- bin/organize-incoming-dcim.ts
+++ bin/organize-incoming-dcim.ts
@@ -9,14 +9,19 @@ import { walk, WalkEntry, WalkOptions } from "https://deno.land/std@0.118.0/fs/w
 
 type FilePath = string;
 
-interface CmdOpts {
+interface OrgIDImportAction {
+	command: "generate-import-script",
 	imageDests : FilePath[];
 	videoDests : FilePath[];
 	inputPaths : FilePath[];
 }
+type OrgIDAction = {
+	command: "print-help"
+} | OrgIDImportAction;
 
-function parseCmdOpts(args:string[]) : CmdOpts {
-	const cmdOpts : CmdOpts = {
+function parseAction(args:string[]) : OrgIDAction {
+	const cmdOpts : OrgIDAction = {
+		command: "generate-import-script",
 		imageDests: [],
 		videoDests: [],
 		inputPaths: [],
@@ -27,6 +32,8 @@ function parseCmdOpts(args:string[]) : CmdOpts {
 			cmdOpts.imageDests.push(m[1]);
 		} else if( (m = /^--video-dest=(.*)/.exec(arg)) != null ) {
 			cmdOpts.videoDests.push(m[1]);
+		} else if( arg == '--help' ) {
+			return { command: "print-help" };
 		} else if( arg.startsWith("-" ) ) {
 			throw new Error("Unrecognized argument: "+arg);
 		} else {
@@ -44,7 +51,7 @@ async function *walkAll( inputs:FilePath[], opts:WalkOptions ) : AsyncIterable<W
 	}
 }
 
-function findInputFiles(cmdOpts:CmdOpts) : AsyncIterable<WalkEntry> {
+function findInputFiles(cmdOpts:OrgIDImportAction) : AsyncIterable<WalkEntry> {
 	return walkAll(cmdOpts.inputPaths, {includeFiles: true, includeDirs: false, followSymlinks: true});
 }
 
@@ -60,7 +67,7 @@ interface ReportUnhandleableFile {
 }
 type Action = LinkFile|ReportUnhandleableFile;
 
-async function *decideWhatToDoWithInputFiles(inputFiles: AsyncIterable<WalkEntry>, cmdOpts:CmdOpts) : AsyncIterable<Action> {
+async function *decideWhatToDoWithInputFiles(inputFiles: AsyncIterable<WalkEntry>, cmdOpts:OrgIDImportAction) : AsyncIterable<Action> {
 	let m:RegExpExecArray|null;
 	for await( const entry of inputFiles ) {
 		if( (m = /^(\d\d\d\d)(\d\d)(\d\d)_(\d\d)(\d\d)(\d\d)\.([^\.]+)$/.exec(entry.name)) ) {
@@ -77,7 +84,6 @@ async function *decideWhatToDoWithInputFiles(inputFiles: AsyncIterable<WalkEntry
 				type == 'image' ? cmdOpts.imageDests :
 				type == 'video' ? cmdOpts.videoDests :
 				[];
-
 			for( const destPath of cmdOpts.imageDests ) {
 				yield {
 					classRef: "http://ns.nuke24.net/SynthGen2100/FSAction/LinkFile",
@@ -89,49 +95,69 @@ async function *decideWhatToDoWithInputFiles(inputFiles: AsyncIterable<WalkEntry
 	}
 }
 
-const cmdOpts = parseCmdOpts(Deno.args);
-const inputFiles = findInputFiles(cmdOpts);
+const cmdOpts = parseAction(Deno.args);
 
-if( cmdOpts.inputPaths.length == 0 ) console.warn("No input paths specified");
-if( cmdOpts.imageDests.length == 0 ) console.warn("No image destination paths specified");
-if( cmdOpts.videoDests.length == 0 ) console.warn("No video destination paths specified");
+const selfName = 'organize-incoming-dcim';
 
-console.log("@echo off");
-
-function dirname(path:FilePath) {
-	let m;
-	if( m = /^(.*?)[\\\/][^\\\/]+$/.exec(path) ) {
-		return m[1];
-	} else {
-		return undefined;
-	}
-}
-
-const mkdirsAlreadyOutput = new Set<FilePath>();
-function mkdir(path:FilePath) {
-	if( mkdirsAlreadyOutput.has(path) ) return;
-	console.log(`IF NOT EXIST "${path}" MKDIR "${path}"`);
-	mkdirsAlreadyOutput.add(path);
-}
-
-for await( const action of decideWhatToDoWithInputFiles(inputFiles, cmdOpts) ) {
-	switch( action.classRef ) {
-	case "http://ns.nuke24.net/TOGoSUtils/OrganizeIncomingDCIM/ReportUnhandleableFile":
-		console.log(`rem Ignoring ${action.path}: ${action.reason}`);
+// TODO: Could be combined with 'doAction' except that one currently
+// doesn't do the actions lol
+async function doOrgidAction( cmdOpts : OrgIDAction ) : Promise<void> {
+	switch( cmdOpts.command ) {
+	case "print-help":
+		console.log(`Usage:`);
+		console.log(`  ${selfName} --help ; show help`);
+		console.log(`  ${selfName} [--image-dest=<dir>] [--video-dest=<dir>] [<input-dir> ...] ; generate script to do the importing`);
 		break;
-	case "http://ns.nuke24.net/SynthGen2100/FSAction/LinkFile":
-		{
-			const targetDirPath = dirname(action.targetPath);
-			if( targetDirPath != undefined ) {
-				mkdir(targetDirPath);
+	case "generate-import-script":
+		const inputFiles = findInputFiles(cmdOpts);
+
+		if( cmdOpts.inputPaths.length == 0 ) console.warn("No input paths specified");
+		if( cmdOpts.imageDests.length == 0 ) console.warn("No image destination paths specified");
+		if( cmdOpts.videoDests.length == 0 ) console.warn("No video destination paths specified");
+
+		console.log("@echo off");
+
+		function dirname(path:FilePath) {
+			let m;
+			if( m = /^(.*?)[\\\/][^\\\/]+$/.exec(path) ) {
+				return m[1];
+			} else {
+				return undefined;
 			}
-			console.log(`IF NOT EXIST "${action.targetPath}" FSUTIL HARDLINK CREATE "${action.targetPath}" "${action.sourcePath}"`);
-			break;
 		}
+
+		const mkdirsAlreadyOutput = new Set<FilePath>();
+		function mkdir(path:FilePath) {
+			if( mkdirsAlreadyOutput.has(path) ) return;
+			console.log(`IF NOT EXIST "${path}" MKDIR "${path}"`);
+			mkdirsAlreadyOutput.add(path);
+		}
+
+		for await( const action of decideWhatToDoWithInputFiles(inputFiles, cmdOpts) ) {
+			switch( action.classRef ) {
+				case "http://ns.nuke24.net/TOGoSUtils/OrganizeIncomingDCIM/ReportUnhandleableFile":
+					console.log(`rem Ignoring ${action.path}: ${action.reason}`);
+					break;
+				case "http://ns.nuke24.net/SynthGen2100/FSAction/LinkFile":
+					{
+						const targetDirPath = dirname(action.targetPath);
+						if( targetDirPath != undefined ) {
+							mkdir(targetDirPath);
+						}
+						console.log(`IF NOT EXIST "${action.targetPath}" FSUTIL HARDLINK CREATE "${action.targetPath}" "${action.sourcePath}"`);
+						break;
+					}
+			}
+		}
+
+		console.log("rem Ha ha, that's right; for now this program just outputs Windows CMD commands");
+		console.log("rem I have wondrous technology in some synthgen library about representing FSActions");
+		console.log("rem and being able to output them as Bash or CMD scripts or just do them.");
+		console.log("rem Publish that on Deno.land and useit.");
+		break;
+	default:
+		console.error(`${selfName}: Oops, bad command name, "${(cmdOpts as any).command}"`);
 	}
 }
 
-console.log("rem Ha ha, that's right; for now this program just outputs Windows CMD commands");
-console.log("rem I have wondrous technology in some synthgen library about representing FSActions");
-console.log("rem and being able to output them as Bash or CMD scripts or just do them.");
-console.log("rem Publish that on Deno.land and useit.");
+await doOrgidAction(cmdOpts);
