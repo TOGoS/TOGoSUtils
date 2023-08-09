af94cbd11fe82069eb11bae0e2c18cd8db4b83ac
TOGoS <togos00@gmail.com>
2023-08-09 12:48:45 -0500

    Use or find TU_BASH_EXE instead of just 'bash'

--- bin/gitpull.bat
+++ bin/gitpull.bat
@@ -1,1 +1,16 @@
-@bash %~dp0\gitpull %*
+@echo off
+
+setlocal
+
+rem If you install WSL, you'll get C:\Windows\system32\bash.exe,
+rem which doesn't play so well with Windows paths.
+rem Could try bashifying the path?  Or using a specially-configured bash,
+rem defaulting to the Git one:
+
+if defined TU_BASH_EXE goto bash_found
+if exist "C:\Program Files\Git\usr\bin\bash.exe" set "TU_BASH_EXE=C:\Program Files\Git\usr\bin\bash.exe"
+if defined TU_BASH_EXE goto bash_found
+set TU_BASH_EXE=bash
+:bash_found
+
+"%TU_BASH_EXE%" %~dp0\gitpull %*
--- bin/gitpush.bat
+++ bin/gitpush.bat
@@ -2,4 +2,15 @@
 
 setlocal
 
-bash %~dp0\gitpush %*
+rem If you install WSL, you'll get C:\Windows\system32\bash.exe,
+rem which doesn't play so well with Windows paths.
+rem Could try bashifying the path?  Or using a specially-configured bash,
+rem defaulting to the Git one:
+
+if defined TU_BASH_EXE goto bash_found
+if exist "C:\Program Files\Git\usr\bin\bash.exe" set "TU_BASH_EXE=C:\Program Files\Git\usr\bin\bash.exe"
+if defined TU_BASH_EXE goto bash_found
+set TU_BASH_EXE=bash
+:bash_found
+
+"%TU_BASH_EXE%" %~dp0\gitpush %*
