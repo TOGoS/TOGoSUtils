14a3db82a4bcd985c87051e2190dcee941008e14
TOGoS <togos00@gmail.com>
2024-09-09 08:17:08 -0500

    figure-mvn-cmd.bat checks for maven 3.9.8

--- bin/figure-mvn-cmd.bat
+++ bin/figure-mvn-cmd.bat
@@ -1,5 +1,6 @@
 if defined MVN_CMD goto mvn_cmd_defined
 if exist C:\Users\stevedxfph\apps\apache-maven-3.8.6\bin\mvn.cmd set "MVN_CMD=C:\Users\stevedxfph\apps\apache-maven-3.8.6\bin\mvn.cmd" && goto mvn_cmd_defined
+if exist C:\Users\stevedxfph\apps\apache-maven-3.9.8\bin\mvn.cmd set "MVN_CMD=C:\Users\stevedxfph\apps\apache-maven-3.9.8\bin\mvn.cmd" && goto mvn_cmd_defined
 
 echo MVN_CMD not defined and couldn't find it.>&2
 exit /B 1
