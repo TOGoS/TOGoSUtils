17804d07c984b809198197905a6cb58ce4c19170
TOGoS <togos00@gmail.com>
2024-10-16 17:33:53 -0500

    Look more places for ucm.cmd

--- bin/startucm.bat
+++ bin/startucm.bat
@@ -2,17 +2,22 @@
 
 setlocal
 
-set "unison_exe=C:\apps\unison-0.5.27\ucm.cmd"
-if exist "%unison_exe%" goto unison_exe_found
+if defined UCM_EXE goto ucm_exe_found
 
-set "unison_exe=C:\apps\unison\ucm.exe"
-if exist "%unison_exe%" goto unison_exe_found
+set "UCM_EXE=C:\apps\unison-0.5.27\ucm.cmd"
+if exist "%UCM_EXE%" goto ucm_exe_found
+
+set "UCM_EXE=C:\apps\unison\ucm.cmd"
+if exist "%UCM_EXE%" goto ucm_exe_found
+
+set "UCM_EXE=C:\apps\unison\ucm.exe"
+if exist "%UCM_EXE%" goto ucm_exe_found
 
 echo Couldn't find ucm.exe or ucm.cmd or whatever>&2
 pause
 exit /B 1
 
 
-:unison_exe_found
+:ucm_exe_found
 
-start alacritty --working-directory C:\Users\TOGoS\stuff\proj\Scratch38-S0010 --title UCM --command "%unison_exe%"
+start alacritty --working-directory C:\Users\TOGoS\stuff\proj\Scratch38-S0010 --title UCM --command "%UCM_EXE%"
