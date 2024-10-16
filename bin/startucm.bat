e0908d44aad852c43008eab225c97de816275e5c
TOGoS <togos00@gmail.com>
2024-10-16 17:24:40 -0500

    startucm: do more finding

--- bin/startucm.bat
+++ bin/startucm.bat
@@ -1,1 +1,18 @@
-start alacritty --working-directory C:\Users\TOGoS\stuff\proj\Scratch38-S0010 --title UCM --command C:\apps\unison\ucm.exe
+@echo off
+
+setlocal
+
+set "unison_exe=C:\apps\unison-0.5.27\ucm.cmd"
+if exist "%unison_exe%" goto unison_exe_found
+
+set "unison_exe=C:\apps\unison\ucm.exe"
+if exist "%unison_exe%" goto unison_exe_found
+
+echo Couldn't find ucm.exe or ucm.cmd or whatever>&2
+pause
+exit /B 1
+
+
+:unison_exe_found
+
+start alacritty --working-directory C:\Users\TOGoS\stuff\proj\Scratch38-S0010 --title UCM --command "%unison_exe%"
