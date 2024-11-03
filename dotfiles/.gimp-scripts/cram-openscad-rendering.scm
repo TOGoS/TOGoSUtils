869048c2ab93af5ff19e086d88c5100167e07710
TOGoS <togos00@gmail.com>
2024-11-02 21:19:18 -0500

    cram-openscad-rendering: Autocrop and add border
    This SUCKED.  Writing GIMP scripts SUCKS.
    The documentation is awful.
    Reloading/rerunning scripts is tedious.
    Error messages are nearly useless.
    Debugging SUCKS.

--- dotfiles/.gimp-scripts/cram-openscad-rendering.scm
+++ dotfiles/.gimp-scripts/cram-openscad-rendering.scm
@@ -1,16 +1,42 @@
+(define (cram-xxx-autocrop theImage)
+	; I've tried this with theDrawable
+	; I've tried iterating over every layer
+	; I've tried different plug-in-autocrop and plug-in-autocrop-layer
+	; plug-in-autocrop-layer does nothing.
+	; plug-in-autocrop crops the image but fails to offset the layer.
+	; what tha fack
+	;
+	; Oh nevermind maybe it does work, but maybe it just wasn't visible
+	; for some reason idk jeez.
+	(for-each
+		(lambda (layer)
+			(plug-in-autocrop-layer 0 theImage layer)
+			(plug-in-autocrop 0 theImage layer)
+		)
+		(vector->list (cadr (gimp-image-get-layers theImage))))
+)
+
 (define (cram-openscad-rendering theImage width height colorCount)
+	(cram-xxx-autocrop theImage)
 	(let*
 		(
 			(oldWidth (car (gimp-image-width theImage)))
 			(oldHeight (car (gimp-image-height theImage)))
 			(squaresize (max oldWidth oldHeight))
+			(mostWidth (* width (/ 15 16.0)))
+			(mostHeight (* height (/ 15 16.0)))
 			(theLayer (car (gimp-image-get-active-layer theImage))) ; Assuming one active layer for now
 		)
 		; (gimp-message (string-append "image: " (number->string theImage) ", oldW/H: " (number->string oldWidth) " " (number->string oldHeight) ", squaresize: " (number->string squaresize)))
 		(gimp-image-resize theImage squaresize squaresize (/ (- squaresize oldWidth) 2) (/ (- squaresize oldHeight) 2))
 		(gimp-layer-resize-to-image-size theLayer) ; Uses the background color to fill
 		(gimp-context-set-interpolation INTERPOLATION-CUBIC)
-		(gimp-image-scale theImage width height)
+		; Scale!
+		(gimp-image-scale theImage mostWidth mostHeight)
+		; Add border!
+		(gimp-image-resize theImage width height (/ (- width mostWidth) 2) (/ (- height mostHeight) 2))
+		(gimp-layer-resize-to-image-size theLayer) ; Uses the background color to fill
+		; Downsample!
 		(gimp-image-convert-indexed theImage
 			CONVERT-DITHER-NONE
 			CONVERT-PALETTE-GENERATE
@@ -18,7 +44,6 @@
 			FALSE
 			TRUE
 			"")
-		(gimp-layer-resize-to-image-size theLayer)
 	)
 )
 
