6dc969e3c594c41e6223e03d0a03b766ab4dd59f
TOGoS <togos00@gmail.com>
2024-11-20 18:13:49 -0600

    figure-jcr36-cmd.bat to check more locations

--- bin/figure-jcr36-cmd.bat
+++ bin/figure-jcr36-cmd.bat
@@ -3,9 +3,14 @@ if not defined JAVA_EXE set JAVA_EXE=java
 if defined JCR36_CP goto define_jcr36_cmd_using_cp
 if defined JCR36_JAR goto define_jcr36_cmd_using_jar
 
+set "JCR36_JAR=%UserProfile%\stuff\proj\JavaCommandRunner36\JCR36.1.30.jar"
+if exist %JCR36_JAR% goto define_jcr36_cmd_using_jar
+
 set "JCR36_JAR=%UserProfile%\stuff\proj\JavaCommandRunner36\JCR36.1.28.jar"
-if not exist %JCR36_JAR% goto fail_no_jar
-goto define_jcr36_cmd_using_jar
+if exist %JCR36_JAR% goto define_jcr36_cmd_using_jar
+
+goto fail_no_jar
+
 
 :define_jcr36_cmd_using_cp
 set "JCR36_CMD="%JAVA_EXE%" -cp "%JCR36_CP%" net.nuke24.jcr36.SimplerCommandRunner"
