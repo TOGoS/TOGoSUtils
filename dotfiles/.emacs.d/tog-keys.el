6e286f003cb40c5361ce30f6dbfae5c9ce71cf07
TOGoS <togos00@gmail.com>
2025-09-02 15:55:58 -0500

    `M-g e r` to visit timelog/README.org

--- dotfiles/.emacs.d/tog-keys.el
+++ dotfiles/.emacs.d/tog-keys.el
@@ -73,6 +73,7 @@
 (global-set-key (kbd "M-g t t") 'visit-tog-tasks)
 (global-set-key (kbd "M-g e b") 'visit-bodylog)
 (global-set-key (kbd "M-g e h") 'visit-self-help)
+(global-set-key (kbd "M-g e r") 'visit-timelog-readme)
 (global-set-key (kbd "M-g a l") 'visit-alvin-log)
 (global-set-key (kbd "M-g k l") 'visit-kia-log)
 (global-set-key (kbd "M-g l n") 'visit-love-notes)
